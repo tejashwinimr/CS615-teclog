c255cc24afac87b68269b5d9839b889c97bf5e58
Michael O'Neill <michaeloneill@Michaels-Mac-Pro.local>
2015-10-08 21:09:27 +0100

    Making DB Connector Object Orientated for yummyness

--- database.php
+++ database.php
@@ -1,131 +1,108 @@
 <?php
 
-function createNote($content) {
-    $host = "us-cdbr-azure-west-c.cloudapp.net";
-    $user = "bcfde92a608269";
-    $pwd = "815873fb";
-    $db = "acsm_8cd991cf173de96";
-    try {
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-        $query = $conn->prepare("INSERT INTO notes (content) VALUES (:content);");
-        $query->bindParam(':content', $content);
-        $query->execute();
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+class Db {
+    
+    protected $con;
+    private $host = "us-cdbr-azure-west-c.cloudapp.net";
+    private $user = "bcfde92a608269";
+    private $pwd = "815873fb";
+    private $db = "acsm_8cd991cf173de96";
+    
+    //Creates a PDO conection & sets error mode to exceptions
+    public function __construct(){
+    
+        try { 
+            $this->con = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pwd); 
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+            $this->con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        } 
+        catch(PDOException $e) { 
+            echo $e->getMessage();
+        }
+        
     }
-}
-
-function getNotes() {
-    $host = "us-cdbr-azure-west-c.cloudapp.net";
-    $user = "bcfde92a608269";
-    $pwd = "815873fb";
-    $db = "acsm_8cd991cf173de96";
-    try{
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-        $query = $conn->prepare("SELECT * FROM notes ORDER BY last_modified DESC;");
-        $query->execute();
-
-        return $query->fetchAll();
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+    
+    //sets the datab to null
+    public function disconect() {
+        
+        $this->con = null;
+        
     }
-}
 
-function getMinId() {
-    $host = "us-cdbr-azure-west-c.cloudapp.net";
-    $user = "bcfde92a608269";
-    $pwd = "815873fb";
-    $db = "acsm_8cd991cf173de96";
-    try{
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-        $query = $conn->prepare("SELECT min(id) FROM notes;");
-        $query->execute();
-
-        return $query->fetch()[0];
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+    function createNote($content) {
+        try {
+            $query = $this->con->prepare("INSERT INTO notes (content) VALUES (:content);");
+            $query->bindParam(':content', $content);
+            $query->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
-}
 
-function getMaxId() {
-    $host = "us-cdbr-azure-west-c.cloudapp.net";
-    $user = "bcfde92a608269";
-    $pwd = "815873fb";
-    $db = "acsm_8cd991cf173de96";
-    try{
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-        $query = $conn->prepare("SELECT max(id) FROM notes;");
-        $query->execute();
-
-        return $query->fetch()[0];
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+    function getNotes() {
+        try{
+            $query = $this->con->prepare("SELECT * FROM notes ORDER BY last_modified DESC;");
+            $query->execute();
+            return $query->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
-}
 
-function isValid($id) {
-    $host = "us-cdbr-azure-west-c.cloudapp.net";
-    $user = "bcfde92a608269";
-    $pwd = "815873fb";
-    $db = "acsm_8cd991cf173de96";
-    try{
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-        $query = $conn->prepare("SELECT * FROM notes WHERE id = :id;");
-        $query->bindParam(':id', $id);
-        $query->execute();
-
-        return count($query->fetchAll()) > 0;
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+    function getMinId() {
+        try{
+            $query = $this->con->prepare("SELECT min(id) FROM notes;");
+            $query->execute();
+            return $query->fetch()[0];
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
-}
 
-function deleteNote($id) {
-    $host = "us-cdbr-azure-west-c.cloudapp.net";
-    $user = "bcfde92a608269";
-    $pwd = "815873fb";
-    $db = "acsm_8cd991cf173de96";
-    try{
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-        $query = $conn->prepare("DELETE FROM notes WHERE id = :id;");
-        $query->bindParam(':id', $id);
-        $query->execute();
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+    function getMaxId() {
+        try{       
+            $query = $this->con->prepare("SELECT max(id) FROM notes;");
+            $query->execute();
+            return $query->fetch()[0];
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
-}
 
-function updateNote($id, $newContent) {
-    $host = "us-cdbr-azure-west-c.cloudapp.net";
-    $user = "bcfde92a608269";
-    $pwd = "815873fb";
-    $db = "acsm_8cd991cf173de96";
-    try{
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-        $query = $conn->prepare("UPDATE notes
-                                       SET content = :content,
-                                           last_modified = CURRENT_TIMESTAMP
-                                       WHERE id = :id");
-        $query->bindParam(':id', $id);
-        $query->bindParam(':content', $newContent);
-        $query->execute();
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+    function isValid($id) {
+        try{
+            $query = $this->con->prepare("SELECT * FROM notes WHERE id = :id;");
+            $query->bindParam(':id', $id);
+            $query->execute();
+            return count($query->fetchAll()) > 0;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
-}
 
+    function deleteNote($id) {
+        try{          
+            $query = $this->con->prepare("DELETE FROM notes WHERE id = :id;");
+            $query->bindParam(':id', $id);
+            $query->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    function updateNote($id, $newContent) {
+        try{
+            $query = $this->con->prepare("UPDATE notes
+                                           SET content = :content,
+                                               last_modified = CURRENT_TIMESTAMP
+                                           WHERE id = :id");
+            $query->bindParam(':id', $id);
+            $query->bindParam(':content', $newContent);
+            $query->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    
+}
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
 require_once "lib/Smarty.class.php";
 require_once "database.php";
 
+//connect to our db
+$db = new Db();
+
 if(isset($_COOKIE['ACTIVE_NOTE_ID'])) {
-    if(!isValid($_COOKIE['ACTIVE_NOTE_ID'])) {
-        setcookie("ACTIVE_NOTE_ID", getMaxId());
-        $activeNoteId = getMaxId();
+    if(!$db->isValid($_COOKIE['ACTIVE_NOTE_ID'])) {
+        setcookie("ACTIVE_NOTE_ID", $db->getMaxId());
+        $activeNoteId = $db->getMaxId();
     } else {
         $activeNoteId = $_COOKIE['ACTIVE_NOTE_ID'];
     }
@@ -14,17 +20,17 @@ if(isset($_COOKIE['ACTIVE_NOTE_ID'])) {
 if(isset($_REQUEST['action'])) {
     switch($_REQUEST['action']) {
         case 'delete':
-            deleteNote($activeNoteId);
-            $newId = getMaxId();
+            $db->deleteNote($activeNoteId);
+            $newId = $db->getMaxId();
             setcookie("ACTIVE_NOTE_ID", $newId);
             $activeNoteId = $newId;
             break;
         case 'update':
-            updateNote($_COOKIE['ACTIVE_NOTE_ID'], $_REQUEST['content']);
+            $db->updateNote($_COOKIE['ACTIVE_NOTE_ID'], $_REQUEST['content']);
             break;
         case 'new':
-            createNote("New note.");
-            $newId = getMaxId();
+            $db->createNote("New note.");
+            $newId = $db->getMaxId();
             setcookie("ACTIVE_NOTE_ID", $newId);
             $activeNoteId = $newId;
             break;
@@ -39,6 +45,9 @@ $template = new Smarty();
 
 if(isset($activeNoteId))
     $template->assign("ACTIVE_NOTE_ID", $activeNoteId);
-$template->assign("notes", getNotes());
+$template->assign("notes", $db->getNotes());
 $template->display('index.tpl');
+
+//disconnect
+$db->disconnect();
 ?>
