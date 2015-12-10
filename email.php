bf09e8070ae23027c9246e31027154db714233de
Michael O'Neill <michaeloneill@Michaels-Mac-Pro.local>
2015-12-10 20:11:22 +0000

    Removing that. And trying json_encode

--- email.php
+++ email.php
@@ -26,11 +26,9 @@
 	// Tell curl to use HTTP POST
 	curl_setopt ($session, CURLOPT_POST, true);
 	// Tell curl that this is the body of the POST
-	curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+	curl_setopt ($session, CURLOPT_POSTFIELDS, json_encode($params));
 	// Tell curl not to return headers, but do return the response
-	curl_setopt($session, CURLOPT_HEADER, false);
-	// Tell PHP not to use SSLv3 (instead opting for TLS)
-	curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+	curl_setopt($session, CURLOPT_HEADER, false)
 	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 
 	// obtain response
